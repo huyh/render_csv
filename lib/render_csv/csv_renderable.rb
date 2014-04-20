9bbb7298538023c77830a2e2d0f9810a94127e04
Huy Hoang <huythieuhoang@gmail.com>
2014-04-21 09:00:35 +1000

    Fix order of including header options

--- lib/render_csv/csv_renderable.rb
+++ lib/render_csv/csv_renderable.rb
@@ -17,7 +17,7 @@ module RenderCsv
       columns += options[:add_methods].map(&:to_s) if options[:add_methods]
 
       CSV.generate(encoding: 'utf-8') do |row|
-        row << columns || options[:headers]
+        row << (options[:headers] || columns)
         self.each do |obj|
           row << columns.map { |c| obj.send(c) }
         end
